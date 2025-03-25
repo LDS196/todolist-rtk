fc24cce1ea505dc38be40ce89a958cd3d3bd787b
LDS196 <lihachevd87@gmail.com>
2025-03-25 12:41:57 +0400

    update key

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,7 +4,7 @@ export const instance = axios.create({
 	baseURL: 'https://social-network.samuraijs.com/api/1.1/',
 	withCredentials: true,
 	headers: {
-		'API-KEY': process.env.NEXT_PUBLIC_API_KEY
+		'API-KEY': '06a76f5c-d1d4-4acf-8b82-4894b111d84f'
 	}
 })
 
